765ab7d1ba1f7f066d14408cdd592cf69cd92ee2
Dongthach <thachdong270293@gmail.com>
2020-07-11 15:04:08 +0700

    Fix not get react routes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 //Connect to mongodb
 mongoose.connect(
   process.env.MONGODB_URI,
@@ -42,11 +42,11 @@ app.post("/secret_key", (req, res) => {
   });
 });
 //Catch 404 error
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 400;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Not found");
+//   error.status = 400;
+//   next(error);
+// });
 //Error handler
 app.use((error, req, res, next) => {
   const err = app.get("env") === "production" ? {} : error;
