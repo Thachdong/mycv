627b73310184af1780a4f4ec0d2ecaf5b97c67cb
Dongthach <thachdong270293@gmail.com>
2020-07-11 14:36:04 +0700

    Fix bug 'Can not get react routes'

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use(express.static(path.join(__dirname, "client", "build")));
 //Connect to mongodb
 mongoose.connect(
   process.env.MONGODB_URI,
